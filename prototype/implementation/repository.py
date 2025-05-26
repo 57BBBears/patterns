d1811437ee613beec57ef199b5381ec05e05f813
57BBBears <seocreomail@gmail.com>
2025-05-26 11:27:39 +0300

    prototype pyright fix

--- prototype/implementation/repository.py
+++ prototype/implementation/repository.py
@@ -1,8 +1,4 @@
-from typing import TypeVar
-
 from implementation.dtos import BaseDTO
-
-T = TypeVar("T", bound=BaseDTO)
 
 
 class PrototypeRepository:
@@ -10,25 +6,25 @@ class PrototypeRepository:
     allows to get them by name or by other attributes."""
 
     def __init__(self):
-        self._prototypes: dict[str, T] = {}
+        self._prototypes: dict[str, BaseDTO] = {}
 
-    def add(self, name: str, prototype: T):
+    def add(self, name: str, prototype: BaseDTO):
         self._prototypes[name] = prototype
 
     def remove(self, name: str):
         self._prototypes.pop(name, None)
 
-    def __getitem__(self, item: str) -> T:
+    def __getitem__(self, item: str) -> BaseDTO:
         return self._prototypes[item].clone()
 
-    def get_true(self) -> list[T]:
+    def get_true(self) -> list[BaseDTO]:
         return [
             proto.clone()
             for proto in self._prototypes.values()
             if getattr(proto, "is_true", None)
         ]
 
-    def get_by_color(self, color: str) -> list[T]:
+    def get_by_color(self, color: str) -> list[BaseDTO]:
         return [
             proto.clone()
             for proto in self._prototypes.values()
